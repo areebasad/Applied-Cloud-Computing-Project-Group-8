dccf2db9d2b0c8a9dfe2cee8ea0fd288020c4e0a
FelixDryselius <felix.luxdryselius.7120@student.uu.se>
2019-10-25 17:39:27 +0000

    changed the db_worker_source_file.sh

--- database_backend/db_worker_source_file.sh
+++ database_backend/db_worker_source_file.sh
@@ -1,9 +1,9 @@
 export DB_WORKER_HOST=localhost
 export DB_DATABASE_NAME=test_insert
-export DB_USER=felix
+export DB_USER=felix_root
 
 echo "Please enter Password for DB_USER: $DB_USER "
 read -sr DB_USER_PASSWORD_INPUT
 export DB_USER_PASSWORD=$DB_USER_PASSWORD_INPUT
 
-export PATH_TO_WORKER_RESULTS = "/home/ubuntu/test_results/"
+export PATH_TO_WORKER_RESULTS=/home/ubuntu/test_results/
