6093e7be88769d2bf10b532fe05d4d5fc56f975c
FelixDryselius <felix.luxdryselius.7120@student.uu.se>
2019-10-28 16:03:00 +0000

    retrieve function now works

--- database_backend/db_broker_source_file.sh
+++ database_backend/db_broker_source_file.sh
@@ -3,7 +3,7 @@
 export DB_HOST_ADDRESS=192.168.1.63
 export DB_DATABASE_NAME=test_insert
 export DB_USER_BROKER=felix_external
-export PATH_TO_DOWNLOADED_RESULTS=/home/ubuntu/test_results/downloaded/
+export PATH_TO_DOWNLOADED_RESULTS=/home/ubuntu/test_download/
 #Password for 'DB_USER'
 echo "Please enter Password for DB_USER: $DB_USER "
 read -sr DB_USER_PASSWORD_INPUT
