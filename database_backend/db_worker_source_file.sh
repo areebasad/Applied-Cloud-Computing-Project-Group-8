9a2650422f3416bf7d2b6a61894b65644e04f5ba
FelixDryselius <felix.luxdryselius.7120@student.uu.se>
2019-10-28 13:07:11 +0100

    started testing for external connection

--- database_backend/db_worker_source_file.sh
+++ database_backend/db_worker_source_file.sh
@@ -1,8 +1,8 @@
 #Run this file on workers that will connect to MariaDB backend
 #Information regarding host, database, user, and path to files
-export DB_WORKER_HOST=localhost
+export DB_WORKER_HOST=192.168.1.28
 export DB_DATABASE_NAME=test_insert
-export DB_USER=felix_root
+export DB_USER=felix_external
 export PATH_TO_WORKER_RESULTS=/home/ubuntu/test_results/
 #Password for 'DB_USER'
 echo "Please enter Password for DB_USER: $DB_USER "
