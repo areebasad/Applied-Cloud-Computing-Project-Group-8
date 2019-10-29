2ee90e6a12552083bf8fb2d21c11aa1c4add6406
YouWuCN <william.wuy.cn@gmail.com>
2019-10-29 17:47:13 +0100

    Added automatic generating xml files

--- flask_app/master.py
+++ flask_app/master.py
@@ -1,35 +1,34 @@
 #!flask/bin/python
-from flask import Flask, jsonify
-from tinydb import TinyDB, Query
-import subprocess
-import os
-import sys
+from flask import Flask
+#from tinydb import TinyDB, Query
+#import subprocess
+#import os
+#import sys
 import readBlob
-from celery.result import AsyncResult
 from tasks import airfoil_calc
 
 app = Flask(__name__)
 
 #START test data and methods
-db = TinyDB('db.json')
+#db = TinyDB('db.json')
 xml_files = ['r2a0n200.xml', 'r3a3n200.xml', 'r1a9n200.xml', 'r0a6n200.xml', 'r3a9n200.xml', 'r3a15n200.xml', 'r3a24n200.xml', 'r1a24n200.xml', 'r2a9n200.xml', 'r2a12n200.xml', 'r3a12n200.xml', 'r2a30n200.xml', 'r2a15n200.xml', 'r1a27n200.xml', 'r0a12n200.xml', 'r0a0n200.xml', 'r3a18n200.xml', 'r1a15n200.xml', 'r0a21n200.xml', 'r1a12n200.xml', 'r2a18n200.xml', 'r2a3n200.xml', 'r3a6n200.xml', 'r1a30n200.xml', 'r0a27n200.xml', 'r2a24n200.xml', 'r1a18n200.xml', 'r3a30n200.xml', 'r2a6n200.xml', 'r3a0n200.xml', 'r2a27n200.xml', 'r0a15n200.xml', 'r0a18n200.xml', 'r0a9n200.xml', 'r0a30n200.xml', 'r1a6n200.xml', 'r0a24n200.xml', 'r3a27n200.xml', 'r1a0n200.xml', 'r3a21n200.xml', 'r2a21n200.xml', 'r1a3n200.xml', 'r1a21n200.xml', 'r0a3n200.xml']
 xml_files.sort()
 
-@app.route('/test', methods=['GET'])
-def airfoil():
-    airfoilDB = Query()
-    final_result = {}
-    for xml_file in xml_files[0:5]:
-        dbResponse = db.search(airfoilDB.filename == xml_file)
-        if (not dbResponse):
-            data = airfoil_calc.delay(xml_file)
-            result = data.get()
-            record = {'filename': xml_file, 'result': result}
-            db.insert(record)
-            final_result[xml_file] = result
-        else:
-            final_result[xml_file] = dbResponse[0]['result']
-    return final_result
+#@app.route('/test', methods=['GET'])
+#def airfoil():
+#    airfoilDB = Query()
+#    final_result = {}
+#    for xml_file in xml_files[0:5]:
+#        dbResponse = db.search(airfoilDB.filename == xml_file)
+#        if (not dbResponse):
+#            data = airfoil_calc.delay(xml_file)
+#            result = data.get()
+#            record = {'filename': xml_file, 'result': result}
+#            db.insert(record)
+#            final_result[xml_file] = result
+#        else:
+#            final_result[xml_file] = dbResponse[0]['result']
+#    return final_result
 
 #END test data and methods
 
@@ -39,7 +38,7 @@ def exact_call(level,angle,node):
     fullFileName = "r{}a{}n{}.xml".format(level,angle,node)
     
     if not readBlob.check_if_result_exists(fullFileName):
-        task = airfoil_calc.delay(fullFileName)
+        airfoil_calc.delay(fullFileName)
 
     print("The result is calculated, contact admin to download.")
     
@@ -58,13 +57,27 @@ def range_call(level,first_angle,last_angle,node):
 
     filesToCalc = xml_files[firstIndex:(lastIndex+1)]
 
-    for file in filesToCalc:            
-        if not readBlob.check_if_result_exists(file):
-            task = airfoil_calc.delay(fullFileName)
+    for fullFileName in filesToCalc:            
+        if not readBlob.check_if_result_exists(fullFileName):
+            airfoil_calc.delay(fullFileName)
     
     print("The result is calculated, contact admin to download.")
 
     return filesToCalc
+
+@app.route('/parameter/<first_angle>/<last_angle>/<ndiv>/<nodes>/<level>', methods=['GET'])
+def user_defined_call(first_angle,last_angle,ndiv,nodes,level):
+    filenames = []
+    step = int(last_angle/ndiv)
+    for l in range(level+1):
+        for a in range(first_angle,last_angle+step,step):
+            filenames.append("r{}a{}n{}.xml".format(l,a,nodes))
+    
+    for filename in filenames:
+        if not readBlob.check_if_result_exists(filename):
+            airfoil_calc.delay(filename)
+    
+    return "The result is calculated, contact admin to download."
 
 
 
--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -15,6 +15,17 @@ celery.conf['CELERY_ACCEPT_CONTENT'] = ['json', 'pickle']
 
 @celery.task
 def airfoil_calc(file_name):
+    #Creating msh files and convert them to xml
+    if not os.path.exists("/home/fenics/shared/murtazo/cloudnaca/msh/"+file_name):
+        level = file_name.split("a")[0][1:]
+        angle = file_name.split("n")[0].split("a")[1]
+        nodes = file_name.split(".")[0].split("n")[1]
+        subprocess.run(["cd","../cloudnaca"])
+        subprocess.run(["./runme.sh", angle, angle, "1", nodes, level])
+        subprocess.run(["cd","msh"])
+        subprocess.run(["dolfin-convert", file_name.split(".")[0]+".msh", file_name])
+        subprocess.run(["cd","../../navier_stokes_solver"])
+    #Do the airfoil
     subprocess.run(["./airfoil", "10", "0.0001", "10.", "0.01", "../cloudnaca/msh/"+file_name])
     currentPath = os.getcwd()
     pathToResults = currentPath +"/results/"
