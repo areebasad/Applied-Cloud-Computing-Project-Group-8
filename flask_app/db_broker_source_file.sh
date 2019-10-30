5b8deb6f3d55288d5170d9fd5ff342148b8535d1
YouWuCN <william.wuy.cn@gmail.com>
2019-10-30 13:42:10 +0000

    modified broker config to DB

--- flask_app/db_broker_source_file.sh
+++ flask_app/db_broker_source_file.sh
@@ -1,10 +1,11 @@
 #Run this file on workers that will connect to MariaDB backend
 #Information regarding host, database, user, and path to files
-export DB_HOST_ADDRESS=192.168.1.63
+export DB_HOST_ADDRESS=130.238.28.158
 export DB_DATABASE_NAME=test_insert
-export DB_USER_BROKER=felix_external
+export DB_USER_BROKER=worker@130.238.28.158
 export PATH_TO_DOWNLOADED_RESULTS=/home/ubuntu/test_download/
 #Password for 'DB_USER'
-echo "Please enter Password for DB_USER: $DB_USER "
-read -sr DB_USER_PASSWORD_INPUT
-export DB_PASSWORD_BROKER=$DB_USER_PASSWORD_INPUT
+export DB_PASSWORD_BROKER=banankaka1
+#echo "Please enter Password for DB_USER: $DB_USER "
+#read -sr DB_USER_PASSWORD_INPUT
+#export DB_PASSWORD_BROKER=$DB_USER_PASSWORD_INPUT
