fdcca9c15f17270484307c9ec60bdceeace3312b
YouWuCN <william.wuy.cn@gmail.com>
2019-10-30 13:36:07 +0000

    fixed bugs

--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -4,7 +4,7 @@ import insertBlob
 import subprocess
 from celery import Celery
 
-CELERY_BROKER_URL = 'pyamqp://airfoil:group8@10.10.10.8/g8host'
+CELERY_BROKER_URL = 'pyamqp://airfoil:group8@10.10.10.10/g8host'
 CELERY_RESULT_BACKEND = 'rpc://'
 
 task_name = sys.argv[0][:-3]
@@ -20,11 +20,11 @@ def airfoil_calc(file_name):
         level = file_name.split("a")[0][1:]
         angle = file_name.split("n")[0].split("a")[1]
         nodes = file_name.split(".")[0].split("n")[1]
-        subprocess.run(["cd","../cloudnaca"])
+        os.chdir("/home/fenics/shared/murtazo/cloudnaca")
         subprocess.run(["./runme.sh", angle, angle, "1", nodes, level])
-        subprocess.run(["cd","msh"])
+        os.chdir("/home/fenics/shared/murtazo/cloudnaca/msh")
         subprocess.run(["dolfin-convert", file_name.split(".")[0]+".msh", file_name])
-        subprocess.run(["cd","../../navier_stokes_solver"])
+        os.chdir("/home/fenics/shared/murtazo/navier_stokes_solver")
     #Do the airfoil
     subprocess.run(["./airfoil", "10", "0.0001", "10.", "0.01", "../cloudnaca/msh/"+file_name])
     currentPath = os.getcwd()
